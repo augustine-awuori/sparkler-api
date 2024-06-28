2ef63cc522dd8fa4c7c270d1e94e9cde835a886e
augustine-awuori <codewithaugustine@gmail.com>
2024-06-28 13:01:44 +0300

    Register user with password

--- models/user.js
+++ models/user.js
@@ -1,32 +1,45 @@
 import mongoose from "mongoose";
 import Joi from "joi";
+import jwt from "jsonwebtoken";
 
-export const User = mongoose.model(
-  "User",
-  new mongoose.Schema({
-    avatar: String,
-    email: {
-      type: String,
-      required: true,
-      unique: true,
+const schema = new mongoose.Schema({
+  avatar: String,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  feedToken: String,
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    minlength: 6,
+    maxlength: 1024,
+    trim: true,
+  },
+  timestamp: {
+    type: Number,
+    default: function () {
+      return this._id.getTimestamp();
     },
-    feedToken: String,
-    name: {
-      type: String,
-      required: true,
-    },
-    timestamp: {
-      type: Number,
-      default: function () {
-        return this._id.getTimestamp();
-      },
-    },
-  })
-);
+  },
+});
+
+schema.methods.generateAuthToken = function () {
+  return jwt.sign(
+    { _id: this._id, email: this.email, name: this.name },
+    process.env.jwtPrivateKey
+  );
+};
+
+export const User = mongoose.model("User", schema);
 
 export const validateUser = (user) =>
   Joi.object({
-    avatar: Joi.string(),
     email: Joi.string().required(),
     name: Joi.string().required(),
+    password: Joi.string(),
   }).validate(user);
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 import express from "express";
+import bcrypt from "bcrypt";
+import _ from "lodash";
 
 import { getUserFeedToken } from "../services/users.js";
 import { User, validateUser } from "../models/user.js";
@@ -8,16 +10,22 @@ import validate from "../middlewares/validate.js";
 const router = express.Router();
 
 router.post("/", validate(validateUser), async (req, res) => {
-  const { avatar, email, name } = req.body;
+  const { email, name, password } = req.body;
 
   let user = await User.findOne({ email });
-  if (user) return res.send(user);
+  if (user) return res.status(400).send({ error: "Email is already taken" });
 
-  user = new User({ avatar, email, name });
+  user = new User({ email, name });
   user.feedToken = getUserFeedToken(user._id);
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(password, salt);
   await user.save();
 
-  res.status(201).send(user);
+  res
+    .status(201)
+    .header("x-auth-token", user.generateAuthToken())
+    .header("access-control-expose-headers", "x-auth-token")
+    .send(_.omit(user, "password"));
 });
 
 router.get("/", async (_req, res) => {
