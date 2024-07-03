f10cd938488b29207069b4d168d8ba4259c262fc
augustine-awuori <codewithaugustine@gmail.com>
2024-07-03 19:49:21 +0300

    Save user's name

--- models/user.js
+++ models/user.js
@@ -3,48 +3,49 @@ import Joi from "joi";
 import jwt from "jsonwebtoken";
 
 const schema = new mongoose.Schema({
-  avatar: String,
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  feedToken: String,
-  name: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    minlength: 6,
-    maxlength: 1024,
-    trim: true,
-  },
-  timestamp: {
-    type: Number,
-    default: function () {
-      return this._id.getTimestamp();
+    avatar: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
     },
-  },
+    feedToken: String,
+    name: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        minlength: 6,
+        maxlength: 1024,
+        trim: true,
+    },
+    timestamp: {
+        type: Number,
+        default: function () {
+            return this._id.getTimestamp();
+        },
+    },
 });
 
 schema.methods.generateAuthToken = function () {
-  return jwt.sign(
-    {
-      _id: this._id,
-      avatar: this.avatar,
-      email: this.email,
-      feedToken: this.feedToken,
-      name: this.name,
-    },
-    process.env.jwtPrivateKey
-  );
+    return jwt.sign(
+        {
+            _id: this._id,
+            avatar: this.avatar,
+            email: this.email,
+            feedToken: this.feedToken,
+            name: this.name,
+        },
+        process.env.jwtPrivateKey
+    );
 };
 
 export const User = mongoose.model("User", schema);
 
 export const validateUser = (user) =>
-  Joi.object({
-    email: Joi.string().required(),
-    password: Joi.string(),
-  }).validate(user);
+    Joi.object({
+        email: Joi.string().required(),
+        name: Joi.string().required(),
+        password: Joi.string(),
+    }).validate(user);
