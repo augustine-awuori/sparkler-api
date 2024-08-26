09827d2dfc6df52b1b8ae8755969379b1febc4eb
augustine-awuori <codewithaugustine@gmail.com>
2024-08-26 13:38:45 +0300

    Save user's chat token when siging in if doesn't have one

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,14 @@
+import { StreamChat } from "stream-chat";
 import express from 'express';
 import bcrypt from "bcrypt";
 
 import { User, validateUser } from "../models/user.js";
 import validator from "../middlewares/validate.js";
+
+const serverClient = StreamChat.getInstance(
+    process.env.chatApiKey,
+    process.env.chatApiSecret
+);
 
 const router = express.Router();
 
@@ -10,6 +16,11 @@ router.post("/", validator(validateUser), async (req, res) => {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (!user) return res.status(404).send({ error: "Email isn't registered." });
+
+    if (!user.chatToken) {
+        user.chatToken = serverClient.createToken(user._id);
+        await user.save();
+    }
 
     const isValidPassword = await bcrypt.compare(password, user.password);
 
