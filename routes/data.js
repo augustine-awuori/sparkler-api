ed923ece8961357589a334d408bbb8454cad3129
augustine-awuori <codewithaugustine@gmail.com>
2024-09-13 18:30:21 +0300

    Change method to retrieve app data

--- routes/data.js
+++ routes/data.js
@@ -2,7 +2,7 @@ import express from "express";
 
 const router = express.Router();
 
-router.post("/", async (_req, res) => {
+router.get("/", async (_req, res) => {
     res.send({ token: process.env.APP_DATA });
 });
 
