2227c60551a6e549c074f8f72d1104e71e0987fc
augustine-awuori <codewithaugustine@gmail.com>
2024-10-22 13:55:22 +0300

    Rename category to department

--- models/course.js
+++ models/course.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 
 export const Course = mongoose.model("Course", new mongoose.Schema({
     lecturer: { type: mongoose.Types.ObjectId, ref: "Person" },
-    category: { type: mongoose.Types.ObjectId, ref: "Category" },
+    department: { type: mongoose.Types.ObjectId, ref: "Category" },
     description: {
         maxlength: 200,
         trim: true,
@@ -28,7 +28,7 @@ export const Course = mongoose.model("Course", new mongoose.Schema({
 export const validateCourse = (course) =>
     Joi.object({
         lecturer: Joi.string().optional(),
-        category: Joi.string().hex().length(24).required(),
+        department: Joi.string().hex().length(24).required(),
         description: Joi.string().max(200).trim().optional().allow(""),
         images: Joi.array().items(Joi.string().uri()).optional(),
         title: Joi.string().min(2).max(50).trim().required(),
