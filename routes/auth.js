645633fb82a379b5f5bbdbc9d5baf7eb64c94b8e
augustine-awuori <codewithaugustine@gmail.com>
2024-10-24 16:56:40 +0300

    Validate user's auth email and password details

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import bcrypt from "bcrypt";
+import Joi from "joi";
 
 import { User } from "../models/user.js";
+import validator from '../middlewares/validate.js';
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+const validateDetails = (details) =>
+    Joi.object({
+        email: Joi.string().required(),
+        password: Joi.string().min(6).required(),
+    }).validate(details);
+
+router.post("/", validator(validateDetails), async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
@@ -16,5 +24,6 @@ router.post("/", async (req, res) => {
         ? res.send(user.generateAuthToken())
         : res.status(400).send({ error: "Invalid username and/or password." });
 });
+
 
 export default router;
