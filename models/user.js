2e920045db0938ba4ffb427f96911139ff2a8521
augustine-awuori <codewithaugustine@gmail.com>
2025-01-09 15:10:42 +0300

    Bug fix: store user's expo-push-token correctly

--- models/user.js
+++ models/user.js
@@ -11,7 +11,7 @@ const schema = new mongoose.Schema({
         required: true,
         unique: true,
     },
-    expoPushToken: String,
+    expoPushToken: Object,
     followers: Object,
     following: Object,
     profileImage: String,
