f5b5a88975074ac748e830df986d30be3309e0fa
augustine-awuori <codewithaugustine@gmail.com>
2025-01-10 18:42:45 +0300

    Bug fix: send notification on sparkler's sparkle quotation

--- routes/sparkles.js
+++ routes/sparkles.js
@@ -80,7 +80,7 @@ router.post("/quote", auth, async (req, res) => {
             foreign_id: userId + time,
             object: `SO:${verb}:${collection.id}`,
             quoted_activity,
-            target: notifyActor ? actorId : `timeline:${userId}`,
+            target: notifyActor ? `notification:${actorId}` : `timeline:${userId}`,
             time,
             to: [...mentionsIdsTags, ...hashtagTags],
             verb,
