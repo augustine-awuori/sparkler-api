b6f389e43530f9eb5e5587660b808bae40c0be01
augustine-awuori <codewithaugustine@gmail.com>
2025-01-28 09:53:23 +0300

    Bug fix: authorise admins

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -1,8 +1,11 @@
-export default (req, res, next) => {
-    const user = req.user;
+import { User } from "../models/user.js";
+
+export default async (req, res, next) => {
+    let user = req.user;
     if (!user) return res.status(400).send({ error: "User does not exist!" });
 
-    if (!user.isAdmin)
+    user = await User.findById(user._id);
+    if (!user?.isAdmin)
         return res.status(403).send({ error: "Unauthorised access" });
 
     next();
