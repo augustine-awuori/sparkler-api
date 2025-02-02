0d83d124dbf3e3cb9791da5a413f6a61875e6797
augustine-awuori <codewithaugustine@gmail.com>
2025-02-02 20:47:13 +0300

    Code refactor: format message

--- routes/mails.js
+++ routes/mails.js
@@ -20,7 +20,7 @@ router.post("/all", [auth, admin], async (req, res) => {
 
 router.post("/incomplete", [auth, admin], async (_req, res) => {
     const emails = (await User.find({}))
-        .filter((user) => (user.authCode))
+        .filter((user) => user.authCode)
         .map((user) => user.email);
 
     if (!emails.length)
@@ -28,8 +28,16 @@ router.post("/incomplete", [auth, admin], async (_req, res) => {
 
     const subject =
         "Reminder: Use Your Sparkler Auth Code & App Update Available";
-    const message =
-        "We noticed that you requested an authentication code but haven’t used it yet. To access your Sparkler account, please use the code sent in the previous email. If the code has expired or you no longer have it, you can request a new one. \n With Auth Code at Sparkler we're going passwordless, you don't need to remember it. \n Additionally, we have an update available on the Play Store with new features and improvements. \n Make sure to update your app for the best experience.";
+    const message = `
+We noticed that you requested an authentication code but haven’t used it yet.  
+To access your Sparkler account, please use the code sent in the previous email.  
+If the code has expired or you no longer have it, you can request a new one.  
+
+With Auth Code at Sparkler, we're going passwordless—you don't need to remember it.  
+
+Additionally, we have an update available on the Play Store with new features and improvements.  
+Make sure to update your app for the best experience.
+`;
     const { accepted } = await sendMail({ to: emails, message, subject });
 
     accepted
