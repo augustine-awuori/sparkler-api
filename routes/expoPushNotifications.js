e27ff13bc81dd0b5a77a0d9e4c72888e000272e8
Scales-Technology <support@scales-technology.co.ke>
2025-07-01 15:44:03 +0300

    Send push notification for resparkles

--- routes/expoPushNotifications.js
+++ routes/expoPushNotifications.js
@@ -2,6 +2,7 @@ import { Expo } from "expo-server-sdk";
 import express from "express";
 import mongoose from "mongoose";
 
+import { getClient } from "../utils/func.js";
 import { User } from "../models/user.js";
 import auth from "../middlewares/auth.js";
 import admin from "../middlewares/admin.js";
@@ -10,21 +11,25 @@ import sendPushNotification from "../utils/pushNotifications.js";
 const router = express.Router();
 
 router.post("/", auth, async (req, res) => {
-  const { message, targetUsersId, title, ...otherProps } = req.body;
+  const { targetUsersId, ...otherProps } = req.body;
 
-  targetUsersId.forEach(async (targetUserId) => {
-    if (!mongoose.isValidObjectId(targetUserId)) return;
+  sendPushNotificationToIds(targetUsersId, { ...otherProps });
 
-    const targetUser = await User.findById(targetUserId);
-    if (!targetUser) return;
+  res.status(201).send();
+});
 
-    const { expoPushToken } = targetUser;
-    sendPushNotificationTo([expoPushToken?.data], {
-      message,
-      title,
-      ...otherProps,
-    });
-  });
+router.post("/:followersOfId", auth, async (req, res) => {
+  const client = getClient();
+  if (!client)
+    return res
+      .status(500)
+      .send({ error: "Error initializing client to send push notifications" });
+
+  const targetUsersId = (
+    await client.feed("user", req.params.followersOfId).followers({})
+  ).results.map(({ feed_id }) => feed_id.replace("timeline:", ""));
+
+  sendPushNotificationToIds(targetUsersId, req.body);
 
   res.status(201).send();
 });
@@ -47,6 +52,18 @@ export function sendPushNotificationTo(
   usersToken.forEach(async (token) => {
     if (Expo.isExpoPushToken(token))
       await sendPushNotification(token, { message, title, ...otherData });
+  });
+}
+
+function sendPushNotificationToIds(targetUsersId = [], notificationData = {}) {
+  targetUsersId.forEach(async (targetUserId) => {
+    if (!mongoose.isValidObjectId(targetUserId)) return;
+
+    const targetUser = await User.findById(targetUserId);
+    if (!targetUser) return;
+
+    const { expoPushToken } = targetUser;
+    sendPushNotificationTo([expoPushToken?.data], notificationData);
   });
 }
 
