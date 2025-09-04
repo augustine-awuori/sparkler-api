ce9df1b3467a17597b601321c015d59dda902771
augustine-awuori <68807737+Augustine95@users.noreply.github.com>
2025-09-04 14:36:44 +0300

    Bug fix: send auth code

--- services/mail.js
+++ services/mail.js
@@ -2,12 +2,15 @@ import nodemailer from "nodemailer";
 import Mailgen from "mailgen";
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
 });
+
 
 function generateHTMLEmail({ message }) {
     const mailGenerator = new Mailgen({
