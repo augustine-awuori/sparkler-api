cfacb28fadfc2ea0b4244afe0748b3fb055dd066
augustine-awuori <68807737+Augustine95@users.noreply.github.com>
2025-12-08 12:33:29 +0300

    Add routes for addition and retrieval of departments

--- models/department.js
+++ models/department.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 
 const schema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-    },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  school: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "School",
+    required: true,
+  },
 });
 
 export const Department = mongoose.model("Department", schema);
---
+++ routes/departments.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+import { Department } from "../models/department.js";
+
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const { name, school } = req.body;
+
+  if (!name || !school)
+    return res.status(400).send({ error: "Name and school are required." });
+
+  res.send(await new Department({ name, school }).save());
+});
+
+router.get("/", async (_req, res) => {
+  const departments = await Department.find({});
+
+  res.send(departments);
+});
+
+export default router;
