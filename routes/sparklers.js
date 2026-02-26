0f88f623c22ed56ebd461403ef240dcfb164b13f
augustine-awuori <68807737+Augustine95@users.noreply.github.com>
2026-02-26 19:38:21 +0300

    Bug fix: capture exeptions and errors

--- routes/sparklers.js
+++ routes/sparklers.js
@@ -16,51 +16,56 @@ const client = new StreamClient(
 const router = express.Router();
 
 router.post("/auth-code/verify", async (req, res) => {
-  const { email, authCode, username, name: newUserName } = req.body;
-  if (!authCode)
-    return res.status(400).send({ error: "Auth code not provided" });
+  try {
+    const { email, authCode, username, name: newUserName } = req.body;
+    if (!authCode)
+      return res.status(400).send({ error: "Auth code not provided" });
 
-  const sparkler = await Sparkler.findOne({ email });
-  if (!sparkler)
-    return res.status(404).send({ error: "Email isn't registered." });
+    const sparkler = await Sparkler.findOne({ email });
+    if (!sparkler)
+      return res.status(404).send({ error: "Email isn't registered." });
 
-  const isValidCode = await bcrypt.compare(authCode, sparkler.authCode);
+    const isValidCode = await bcrypt.compare(authCode, sparkler.authCode);
 
-  if (!isValidCode)
-    return res
-      .status(400)
-      .send({ error: "Invalid username and/or authentication code." });
+    if (!isValidCode)
+      return res
+        .status(400)
+        .send({ error: "Invalid username and/or authentication code." });
 
-  const { name, id, image } = sparkler;
-  const feedToken = createUserToken(id.toString());
+    const { name, id, image } = sparkler;
+    const feedToken = createUserToken(id.toString());
 
-  if (sparkler.custom.invalid) {
-    await client.upsertUsers([
-      {
-        id,
-        name,
-        image,
-        custom: {
-          ...sparkler.custom,
-          invalid: false,
-          username: username
-            ? await findUniqueUsername(username)
-            : sparkler.custom.username,
-          feedToken,
+    if (sparkler.custom.invalid) {
+      await client.upsertUsers([
+        {
+          id,
+          name,
+          image,
+          custom: {
+            ...sparkler.custom,
+            invalid: false,
+            username: username
+              ? await findUniqueUsername(username)
+              : sparkler.custom.username,
+            feedToken,
+          },
         },
-      },
-    ]);
-    if (username)
-      sparkler.custom.username = await findUniqueUsername(username || name);
-    if (newUserName) sparkler.name = newUserName || name;
+      ]);
+      if (username)
+        sparkler.custom.username = await findUniqueUsername(username || name);
+      if (newUserName) sparkler.name = newUserName || name;
+    }
+
+    sparkler.custom = { ...sparkler.custom, feedToken, invalid: false };
+    sparkler.authCode = "";
+    await sparkler.save();
+
+    const token = sparkler.generateAuthToken();
+    res.header("x-auth-token", token).send(token);
+  } catch (error) {
+    console.error(`Error verifying user auth code: ${error}`);
+    res.status(500).send({ error: "Error verifying user auth code " + error });
   }
-
-  sparkler.custom = { ...sparkler.custom, feedToken, invalid: false };
-  sparkler.authCode = "";
-  await sparkler.save();
-
-  const token = sparkler.generateAuthToken();
-  res.header("x-auth-token", token).send(token);
 });
 
 router.post("/auth-google", async (req, res) => {
@@ -91,31 +96,38 @@ router.post("/auth-google", async (req, res) => {
 });
 
 router.post("/auth-code", async (req, res) => {
-  const { email } = req.body;
+  try {
+    const { email } = req.body;
 
-  let user = await Sparkler.findOne({ email });
-  const authCode = getAuthCode();
-  const salt = await bcrypt.genSalt(10);
+    let user = await Sparkler.findOne({ email });
+    const authCode = getAuthCode();
+    const salt = await bcrypt.genSalt(10);
 
-  const hashedAuthCode = await bcrypt.hash(authCode.toString(), salt);
+    const hashedAuthCode = await bcrypt.hash(authCode.toString(), salt);
 
-  if (!user) {
-    const name = email.replace("@gmail.com", "");
-    user = new Sparkler({ email, name, custom: { invalid: true } });
+    if (!user) {
+      const name = email.replace("@gmail.com", "");
+      user = new Sparkler({ email, name, custom: { invalid: true } });
+    }
+
+    user.authCode = hashedAuthCode;
+    await user.save();
+
+    const response = await sendMail({
+      message: `Your one time authentication code is: ${authCode}`,
+      subject: "Login Auth-Code",
+      to: email,
+    });
+
+    response?.data
+      ? res.send({ message: "Code has been sent to the email provided" })
+      : res.status(500).send({ error: response?.error });
+  } catch (error) {
+    console.error(`Error sending auth code ${error}`);
+    res.status(500).send({
+      error: "Something happened while sending the auth code " + error,
+    });
   }
-
-  user.authCode = hashedAuthCode;
-  await user.save();
-
-  const response = await sendMail({
-    message: `Your one time authentication code is: ${authCode}`,
-    subject: "Login Auth-Code",
-    to: email,
-  });
-
-  response?.data
-    ? res.send({ message: "Code has been sent to the email provided" })
-    : res.status(500).send({ error: response?.error });
 });
 
 router.post("/", async (req, res) => {
