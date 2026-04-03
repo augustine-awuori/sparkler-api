a982f45eb2fdf8b1b790c4eab2d038cf74c52ace
augustine-awuori <68807737+Augustine95@users.noreply.github.com>
2026-04-03 10:06:34 +0300

    Bug fix: use the right DB

--- startup/db.js
+++ startup/db.js
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
 
-const db =
-  "mongodb+srv://campus-hub-api:0C5IBWgAdMlc1Xzj@cluster0.9iufxoq.mongodb.net/";
+const db = process.env.db;
 
 export default function () {
   if (!db) return console.error("Invalid database connection string");
 
   mongoose
-    .connect(db)
+    .connect(db, {
+      serverSelectionTimeoutMS: 10000,
+      socketTimeoutMS: 45000,
+      retryWrites: true,
+      w: "majority",
+    })
     .then(() => console.log(`Connection to database is successful!`))
     .catch((error) => console.error(`Error connecting to the DB ${error}`));
 }
